5bae36a15c107ee9d1ac5504a802afdb3e1c39ef
Pham Long Hai <85566425+beyond089@users.noreply.github.com>
2023-10-05 17:48:41 +0700

    Update scritp.js

--- scritp.js
+++ scritp.js
@@ -41,3 +41,8 @@ function enableRightClick() {
 function disableRightClick() {
   isRightClickAllowed = false;
 }
+
+//F12 
+window.addEventListener('keydown', (e) => {
+    if(e.key === 'F12') e.preventDefault();
+})
