5165ba41a57323d9bab351458c4fbfd6af08dfc1
Pham Long Hai <85566425+beyond089@users.noreply.github.com>
2023-10-05 17:32:58 +0700

    Update scritp.js

--- scritp.js
+++ scritp.js
@@ -11,3 +11,33 @@ function hoverOn() {
 function hoverOut() {
   document.querySelector('h1').innerHTML = 'Happy Birthday Cubi';
 }
+
+//Can't right click
+let isRightClickAllowed = false;
+
+// Get body tag
+const body = document.body;
+
+body.addEventListener('contextmenu', handleRightClick);
+
+function handleRightClick(e) {
+
+  if(!isRightClickAllowed) {
+
+    e.preventDefault();
+
+    e.stopPropagation();
+
+    return;
+
+  }
+
+}
+
+function enableRightClick() {
+  isRightClickAllowed = true;
+}
+
+function disableRightClick() {
+  isRightClickAllowed = false;
+}
